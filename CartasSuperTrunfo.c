3fceb288e868cfa377a2f19ccb824628f9277dc2
jssantos1229-sketch <jssantos1229@gmail.com>
2025-12-14 02:35:50 +0000

    Novo Commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,83 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+printf("Desafio Trunfo\n");
+printf("Novo Commit\n");
 
-  // Área para entrada de dados
+char estado[50];
+char codigo[5];
+char cidade[50];
+int populacao;
+float area;
+float pib;
+int pontosTuristicos;
+printf("\nInsira os dados da primeira carta 1:\n");
 
-  // Área para exibição dos dados da cidade
+printf("Nome do estado: ");
+scanf(" %[^\n]", estado);
+
+printf("Código da Carta (ex: A01): ");
+scanf("%s", codigo);
+printf("Nome da Cidade: ");
+scanf(" %s", cidade);
+
+printf("População: ");
+scanf("%d", &populacao);
+
+printf("Área em km²: ");
+scanf("%f", &area);
+
+printf("PIB (em bilhões): ");
+scanf("%f", &pib);
+
+printf("Número de pontos turísticos: ");
+scanf("%d", &pontosTuristicos);
+
+    // ===== SAÍDA  1 =====
+printf("\n===== CARTA CADASTRADA 1 =====\n");
+printf("Estado: %s\n", estado);
+printf("Código: %s\n", codigo);
+printf("Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2f bilhões\n", pib);
+printf("Pontos Turísticos: %d\n", pontosTuristicos);
+
+ // ===== ENTRADA DA CARTA 2 =====
+printf("\nInsira os dados da segunda carta 2:\n");
+
+printf("Nome do estado: ");
+scanf(" %[^\n]", estado);
+
+printf("Código da Carta (ex: A02): ");
+scanf("%s", codigo);
+
+printf("Nome da Cidade: ");
+scanf(" %[^\n]", cidade);
+
+printf("População: ");
+scanf("%d", &populacao);
+
+printf("Área em km²: ");
+scanf("%f", &area);
+
+printf("PIB (em bilhões): ");
+scanf("%f", &pib);
+
+printf("Número de pontos turísticos: ");
+scanf("%d", &pontosTuristicos);
+
+    // ===== SAÍDA 2 =====
+printf("\n===== CARTA CADASTRADA 2 =====\n");
+printf("Estado: %s\n", estado);
+printf("Código: %s\n", codigo);
+printf("Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Área: %.2f km²\n", area);
+printf("PIB: %.2f bilhões\n", pib);
+printf("Pontos Turísticos: %d\n", pontosTuristicos);
 
 return 0;
-} 
+
+}
+ 
